75f13f97b7131cd6670a65b1581d87b0ded3e81b
Bruno Alonso Palma Ávila <bpalma@latinshare.com>
2017-08-13 02:45:42 -0300

    error circunscripcion

--- LeerPdf/Form1.cs
+++ LeerPdf/Form1.cs
@@ -11,6 +11,7 @@ using System.IO;
 using iTextSharp.text.pdf;
 using iTextSharp.text.pdf.parser;
 using System.Text.RegularExpressions;
+using System.Data.SqlClient;
 
 namespace LeerPdf
 {
@@ -24,6 +25,15 @@ namespace LeerPdf
         private void btnProcesar_Click(object sender, EventArgs e)
         {
             int nro = 0;
+            DataTable workTable = new DataTable("Persona");
+            
+
+            workTable.Columns.Add("Nombre", typeof(String));
+            workTable.Columns.Add("Rut", typeof(String));
+            workTable.Columns.Add("Ciudad", typeof(String));
+            workTable.Columns.Add("Genero", typeof(String));
+            workTable.Columns.Add("Direccion", typeof(String));
+
             string rutaArchivos = @"C:\Users\BrunoAlonsoPalmaÁvil\OneDrive\Leer PDF excel\Nueva carpeta\Datos comunas\";
             foreach (string file in Directory.EnumerateFiles(rutaArchivos, "*.pdf"))
             {
@@ -59,7 +69,7 @@ namespace LeerPdf
                 {
                     Regex regex = new Regex(@"\d{1,2}.\d{3}.\d{3}-\d{1}");
                     //Match match = regex.Match(arrString[i]);
-                    string pattern = @"(\s\d)([a-zA-Z\s]*)(\d{1,2}.\d{3}.\d{3}-\d{1})\s(MUJ|VAR)\s([a-zA-Z0-9,.!? ]*)("+ciudad+@")\s(\d*\s*\s(V|M))";
+                    string pattern = @"(\s\d*)([a-zA-Z\s]*)(\d{1,2}.\d{3}.\d{3}-\d{1})\s(MUJ|VAR)\s([a-zA-Z0-9,.!? ]*)("+ciudad+@")\s(\d*\s*\s(V|M))";
                     MatchCollection matches = Regex.Matches(arrString[i], pattern);
 
                     foreach (Match match in matches)
@@ -70,6 +80,22 @@ namespace LeerPdf
                         var direccion = match.Groups[5].Value;
                         var ciudadP = match.Groups[6].Value;
                         nro++;
+
+
+                        DataRow workRow;
+                        if (nombre == "")
+                        {
+                            var asdx = "";
+                        }
+
+                        workRow = workTable.NewRow();
+                        workRow["Nombre"] = nombre;
+                        workRow["Rut"] = rut;
+                        workRow["Genero"] = genero;
+                        workRow["Direccion"] = direccion;
+                        workRow["Ciudad"] = ciudad;
+                        workTable.Rows.Add(workRow);
+
                     }
                     //if (match.Success)
                     //{
@@ -79,6 +105,33 @@ namespace LeerPdf
                 
                 
             }
+            String connString = "Server=tcp:sqllatin.database.windows.net,1433;Initial Catalog=sqllatin;Persist Security Info=False;User ID=latinadmin;Password=Latin123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            // connect to SQL
+            using (SqlConnection connection =
+                    new SqlConnection(connString))
+            {
+                // make sure to enable triggers
+                // more on triggers in next post
+                SqlBulkCopy bulkCopy =
+                    new SqlBulkCopy
+                    (
+                    connection,
+                    SqlBulkCopyOptions.TableLock |
+                    SqlBulkCopyOptions.FireTriggers |
+                    SqlBulkCopyOptions.UseInternalTransaction,
+                    null
+                    );
+
+                // set the destination table name
+                bulkCopy.DestinationTableName = "Persona";
+                connection.Open();
+
+                // write the data in the "dataTable"
+                //bulkCopy.WriteToServer(workTable);
+                connection.Close();
+            }
+            // reset
+            //this.dataTable.Clear();
             var asdf = "";
 
         }
